349989266b184a5589add5e689ba86646ce41ed7
Kseniia Lebedeva <lightgraywrite@gmail.com>
2023-04-03 19:00:17 -0700

    Fixed CORS policy error on the FE, updated gradle dependencies, added Spring Security.
    Signed-off-by: Kseniia Lebedeva <lightgraywrite@gmail.com>

--- src/main/java/io/chatasticlogin/config/WebSecurityConfig.java
+++ src/main/java/io/chatasticlogin/config/WebSecurityConfig.java
@@ -1,26 +1,57 @@
 package io.chatasticlogin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class WebSecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
-//        // temporary disables login page and web security
-//        security.csrf().disable().authorizeHttpRequests()
-//                .requestMatchers("/swagger", "/swagger-ui/index.html").permitAll();
-////                .and()
-////                .httpBasic()
-////                .disable();
-//        return security.build();
-//    }
+    @Value("${frontend.api}")
+    private String frontend_api;
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**", "/swagger.json",
+                "/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/swagger-ui/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/swagger", "/swagger-ui/index.html").permitAll()
+                .requestMatchers("/api/v1/**").permitAll();
+        return security.build();
+    }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Creates and configures a CorsFilter bean for handling Cross-Origin Resource Sharing (CORS) requests.
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(frontend_api));
+        config.setAllowedMethods(List.of("GET", "POST"));
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
